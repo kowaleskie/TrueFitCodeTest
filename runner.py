892c7f650d2f493f79704694779fe992b57357db
Graduate <evan.kowaleski@gmail.com>
2018-10-07 20:35:25 -0400

    Adding files

---
+++ runner.py
@@ -0,0 +1,50 @@
+import unittest
+import json
+from selenium import webdriver
+
+browsers = {"windows": {"chrome": r'drivers\windows\chromedriver.exe',
+                    "firefox": r'drivers\windows\geckodriver.exe',
+                    "edge": r'drivers\windows\MicrosoftWebDriver.exe'},
+
+            "mac": {"chrome": r'drivers/mac/chromedriver',
+                    "firefox": r'drivers/mac/geckodriver'},
+
+            "linux": {"chrome": r'drivers/linux/chromedriver',
+                      "firefox": r'drivers/linux/geckodriver'}
+            }
+
+
+class TestRunner():
+    def __init__(self):
+
+        self.loader = unittest.TestLoader()
+        self.suite = self.loader.discover('tests', pattern='*.py')
+        self.runner = unittest.TextTestRunner()
+        self.runner.run(self.suite)
+
+
+class MakeDriver():
+    def __init__(self):
+        with open('settings.json') as f:
+            settings = json.load(f)
+        os = settings["os"]
+
+        try:
+            if settings['browser'].lower() == "chrome":
+                self.driver = webdriver.Chrome(executable_path=browsers[os]["chrome"])
+            elif settings['browser'].lower() == "firefox":
+                self.driver = webdriver.Firefox(executable_path=browsers[os]["firefox"])
+            elif settings['browser'].lower() == "edge":
+                self.driver = webdriver.Edge(executable_path=browsers["win"]["edge"])
+        except:
+            self.driver = webdriver.Chrome(executable_path=browsers["mac"]["chrome"])
+
+        self.driver.set_page_load_timeout(10)
+        self.driver.get("http://www.google.com")
+
+    def __del__(self):
+        self.driver.close()
+
+
+if __name__ == '__main__':
+    TR = TestRunner()
---
+++ tests/google_tests.py
@@ -0,0 +1,88 @@
+import unittest
+import time
+
+from runner import MakeDriver
+from selenium.common.exceptions import NoSuchElementException, ElementNotVisibleException, \
+    ElementNotInteractableException, WebDriverException
+
+browser = MakeDriver()
+google_elements = {"logo_id": "hplogo",
+                   "search_box_name": "q",
+                   "search_button_name": "btnK"}
+
+
+class GoogleTests(unittest.TestCase):
+    def __init__(self, methodName='test'):
+        super(GoogleTests, self).__init__(methodName)
+
+    def setUp(self):
+        browser.driver.get("http://www.google.com")
+
+    def test_google_check_logo(self):
+        self.check_exists_by_id(google_elements["logo_id"], "Logo")
+
+    def test_google_search_box(self):
+
+        self.check_exists_by_name(google_elements["search_box_name"], "Search Box")
+
+    def test_google_search_button(self):
+
+        self.check_exists_by_name(google_elements["search_button_name"], "Search Button")
+
+    def test_google_text_field(self):
+
+        current_text = self.send_text("True Fit")
+        self.assertEqual("True Fit", current_text)
+
+    def test_google_search_results(self):
+
+        self.send_text("True Fit", search=True)
+
+        boxes = browser.driver.find_element_by_xpath('//*[@id="nav"]/tbody/tr')
+        links = boxes.find_elements_by_css_selector('a')
+        results = []
+        for link in links:
+            results.append(link.get_attribute('href'))
+        self.assertGreater(len(results), 0)
+
+    def test_google_search_blank_text_field(self):
+        browser.driver.find_element_by_name(google_elements["search_box_name"]).clear()
+        try:
+            browser.driver.find_element_by_name(google_elements["search_button_name"]).click()
+        except ElementNotVisibleException:
+            return
+        except ElementNotInteractableException:
+            return
+        except WebDriverException:
+            return
+        self.assertTrue(False, "Was able to search blank field")
+
+    def check_exists_by_name(self, element_name, element):
+        try:
+            browser.driver.find_element_by_name(element_name)
+        except NoSuchElementException:
+            self.assertTrue(False, "Google {} does not exist".format(element))
+        self.assertTrue(True, "Google {} exists".format(element))
+
+    def check_exists_by_id(self, element_id, element):
+        try:
+            browser.driver.find_element_by_id(element_id)
+        except NoSuchElementException:
+            self.assertTrue(False, "Google {} does not exist".format(element))
+
+    def send_text(self, text, search=False):
+
+        try:
+            browser.driver.find_element_by_name(google_elements["search_box_name"]).clear()
+            browser.driver.find_element_by_name(google_elements["search_box_name"]).send_keys(text)
+            time.sleep(1)
+            if search:
+                browser.driver.find_element_by_name(google_elements["search_button_name"]).click()
+        except ElementNotVisibleException:
+                self.assertTrue(False, "Element not interactable")
+
+        return browser.driver.find_element_by_name(google_elements["search_box_name"]).get_attribute('value')
+
+
+if __name__ == '__main__':
+    unittest.main()
