3f36b3dd1e7f3a046a29d8a7fdf8d4a1e50eaace
Graduate <evan.kowaleski@gmail.com>
2018-10-07 23:56:08 -0400

    Added comments

--- tests/google_tests.py
+++ tests/google_tests.py
@@ -6,6 +6,8 @@ from selenium.common.exceptions import NoSuchElementException, ElementNotVisible
     ElementNotInteractableException, WebDriverException
 
 browser = MakeDriver()
+
+# Uses ID/name to identify elements as they're more reliable than xpath.
 google_elements = {"logo_id": "hplogo",
                    "search_box_name": "q",
                    "search_button_name": "btnK"}
@@ -13,12 +15,15 @@ google_elements = {"logo_id": "hplogo",
 
 class GoogleTests(unittest.TestCase):
     def __init__(self, methodName='test'):
+
         super(GoogleTests, self).__init__(methodName)
 
     def setUp(self):
+
         browser.driver.get("http://www.google.com")
 
     def test_google_check_logo(self):
+
         self.check_exists_by_id(google_elements["logo_id"], "Logo")
 
     def test_google_search_box(self):
@@ -38,15 +43,20 @@ class GoogleTests(unittest.TestCase):
 
         self.send_text("True Fit", search=True)
 
+        # check if google search results are present
         boxes = browser.driver.find_element_by_xpath('//*[@id="nav"]/tbody/tr')
         links = boxes.find_elements_by_css_selector('a')
         results = []
+
         for link in links:
             results.append(link.get_attribute('href'))
         self.assertGreater(len(results), 0)
 
     def test_google_search_blank_text_field(self):
+
         browser.driver.find_element_by_name(google_elements["search_box_name"]).clear()
+
+        # Different browsers produce different exceptions.
         try:
             browser.driver.find_element_by_name(google_elements["search_button_name"]).click()
         except ElementNotVisibleException:
@@ -58,19 +68,32 @@ class GoogleTests(unittest.TestCase):
         self.assertTrue(False, "Was able to search blank field")
 
     def check_exists_by_name(self, element_name, element):
+        """"
+        :param element_name: The elements name that will be used to identify.
+        :parameter element: Element for error reporting.
+        """
+
         try:
             browser.driver.find_element_by_name(element_name)
         except NoSuchElementException:
             self.assertTrue(False, "Google {} does not exist".format(element))
-        self.assertTrue(True, "Google {} exists".format(element))
 
     def check_exists_by_id(self, element_id, element):
+        """"
+        :param element_id: The elements name that will be used to identify.
+        :parameter element: Element for error reporting.
+        """
+
         try:
             browser.driver.find_element_by_id(element_id)
         except NoSuchElementException:
             self.assertTrue(False, "Google {} does not exist".format(element))
 
     def send_text(self, text, search=False):
+        """"
+        :param text: Text to type in search bar
+        :param search: Condition on if search is entered or not.
+        """
 
         try:
             browser.driver.find_element_by_name(google_elements["search_box_name"]).clear()
